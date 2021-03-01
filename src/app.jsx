77990b8e36b6a39ee6a8fd1412b09e121769568c
tjdgns5272 <tjdgns5272@naver.com>
2021-03-01 21:05:38 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './app.css';
+import VideoList from "./components/video_list";
 
 function App() {
-  return <h1>Hello :)</h1>
 
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAvsKgkC1UZlZMVtg-sU7lMzB3YxplC93M", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  },[])
+  return <VideoList videos={videos}/>
 }
-
 export default App;
